06ae57ebf60e36a5eb96ac5a6b821723b13fb1ea
moshiur <howladermoshiur@gmail.com>
2024-12-28 07:08:17 -0500

    Assignment 8 Complete 🎉

---
+++ base_external/package/aesd-char-driver/aesd-char-driver.mk
@@ -0,0 +1,28 @@
+
+##############################################################
+#
+# AESD_CHAR_DRIVER
+#
+##############################################################
+
+#TODO: Fill up the contents below in order to reference your assignment 3 git contents
+AESD_CHAR_DRIVER_VERSION = 0b1c0f397c765b3cfdc2f7c318145a525580122a #git rev-parse <commit> to get full commit version id 
+#the commit used is for commit representing a working fixed a4p2 finder-test.sh in
+#https://github.com/cu-ecen-aeld/assignments-3-and-later-Mosh333/commits/main/
+# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
+# with ssh keys and the automated build/test system.
+# Your site should start with git@github.com:
+AESD_CHAR_DRIVER_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-Mosh333.git
+AESD_CHAR_DRIVER_SITE_METHOD = git
+AESD_CHAR_DRIVER_GIT_SUBMODULES = YES
+
+AESD_CHAR_DRIVER_MODULE_SUBDIRS = aesd-char-driver
+
+define AESD_CHAR_DRIVER_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_load $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_unload $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment8/* $(TARGET_DIR)/usr/bin
+endef
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
